4bdaff4a541f4206d14fe5e01082933eddb325a8
Thijs Boom <twboom@hotmail.nl>
2020-12-06 23:06:19 +0100

    Removed fetch from status

--- statustoday/js/status.js
+++ statustoday/js/status.js
@@ -24,22 +24,6 @@ function status(url,file,callback) {
   img.src = url+'/'+file
 }
 
-// Implementing the function on the website
-function fetch(url,file) {
-status(url,file,function(found){
 
-  // When the function returns that images is found
-  if(found) {
-    document.querySelector('#outputText').innerHTML = 'online';
-    console.log('found');
-  }
-
-  // When the function returns that images isn't found
-  else {
-    // TODO: Implement some kind of error
-    console.log('error')
-  }
-})
-};
 
 //window.onload = fetch;
