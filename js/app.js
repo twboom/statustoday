52373085e94187ba1fd1b07d1f79706ff647b864
Thijs Boom <twboom@hotmail.nl>
2021-04-19 00:43:52 +0200

    Added auto rechecker

--- js/app.js
+++ js/app.js
@@ -1,11 +1,11 @@
 const config = {
     "url": "https://somtoday.nl",
     "files": ["favicon-16x16.png", "favicon-32x32.png", "favicon-96x96.png", "favicon-160x160.png", "favicon-192x192.png"],
-    "timeout": 5000
-}
+    "timeout": 5000,
+    "interval": 600000
+};
 
-const session = []
-
+let session = [];
 const status = [];
 
 status.get = async function(file, timeout, callback) {
@@ -30,16 +30,15 @@ status.get = async function(file, timeout, callback) {
 }
 
 status.display = function() {
+    const total = session.length;
+    const map = session.reduce(function(obj, b) {
+        obj[b] = ++obj[b] || 1;
+        return obj;
+    }, {});
+
+    console.log(`${map.true} of ${total} loaded`)
+    const loaded = 100 - map.true / total * 100;
     if (session.includes(true)) {
-        const total = session.length;
-        const map = session.reduce(function(obj, b) {
-            obj[b] = ++obj[b] || 1;
-            return obj;
-          }, {});
-
-        console.log(`${map.true} of ${total} loaded`)
-        const loaded = 100 - map.true / total * 100;
-
         document.getElementById('status-display').innerText = ''
 
         if (map.true < session.length) {
@@ -49,11 +48,16 @@ status.display = function() {
         document.getElementById('status-display').innerText += 'online';
         document.getElementById('error-rate-display').innerText = `${loaded}%`
     } else {
-        document.getElementById('status-display').innerText += 'probably offline';
-    }
-}
+        document.getElementById('status-display').innerText + 'probably offline';
+    };
+    const now = new Date();
+    status.lastCheck = Date.now();
+    document.getElementById('last-check').innerText = `${now.getHours()}:${now.getMinutes()}:${now.getSeconds()},${now.getMilliseconds()}`
+};
 
-status.check = function() {
+status.check = async function() {
+
+    session = [];
 
     for (let i = 0; i < config.files.length; i++) {
         const file = `${config.url}/${config.files[i]}`
@@ -66,10 +70,21 @@ status.check = function() {
 
     const interval = setInterval(_ => {
         if (session.length === config.files.length) {
-            clearInterval(interval)
-            status.display()
+            clearInterval(interval);
+            console.log(7)
+            status.display();
         }
     })
-}
+};
 
-window.onload = status.check;
+window.onload = async function() {
+    status.check();
+
+    status.interval = setInterval(_ => {
+        status.check();
+    }, config.interval);
+
+    status.displayUpdate = setInterval(_ => {
+        document.getElementById('since-last-check').innerText = `${(Date.now() - status.lastCheck) / 1000}`
+    }, 10)
+}
